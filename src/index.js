d2af6674c75ed952e7baab27ec78587f312e84fc
Mathew White <88343925+MithrilPebel@users.noreply.github.com>
2021-08-19 20:41:40 -0700

    Created Command Handler

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 console.clear();
 
-const Discord = require("discord.js");
+const Client = require("./Structures/Client");
+const Command = require("./Structures/Command");
 
 const config = require("./Data/config.json");
 
-const intents = new Discord.Intents(32767);
+const client = new Client();
 
-const client = new Discord.Client({ intents });
+const fs = require("fs");
+
+fs.readdirSync("./src/Commands")
+    .filter(file => file.endsWith(".js"))
+    .forEach(file => {
+        /**
+         * @type {Command}
+         */
+        const command = require(`./Commands/${file}`);
+        console.log(`Command ${command.name} loaded`);
+        client.commands.set(command.name, command);
+})
 
 client.on("ready", () => console.log("BronyBot is Online!"));
+
+client.on("messageCreate", message => {
+
+    if (!message.content.startsWith(config.prefix)) return;
+
+    const args = message.content.substring(config.prefix.length).split(/ +/);
+
+    const command = client.commands.find(cmd => cmd.name == args[0]);
+
+    if (!command) return message.reply(`${args[0]} is not a valid command!`);
+
+    command.run(message, args, client);
+
+})
 
 client.login(config.token);
